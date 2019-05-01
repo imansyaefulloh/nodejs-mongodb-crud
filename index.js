b527b6621328e9d7d8ddec4f9c9970f61a958737
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 21:32:18 +0700

    insert single document

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const mongoURI = 'mongodb://localhost:27017/';
 MongoClient.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
   if (err) throw err;
   let dbo = db.db('nodejs_mongodb_crud');
-  dbo.createCollection("customers", (err, res) => {
+  let myObj = { name: 'Company Inc', address: 'Highway 37'};
+  dbo.collection("customers").insertOne(myObj, (err, res) => {
     if (err) throw err;
-    console.log('Collection created');
+    console.log('1 document inserted');
     db.close();
   });
 });
