c3a061156bd9aebc904a3eb9525c55678544d431
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 21:35:17 +0700

    insert multiple documents

--- index.js
+++ index.js
@@ -6,10 +6,37 @@ const mongoURI = 'mongodb://localhost:27017/';
 MongoClient.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
   if (err) throw err;
   let dbo = db.db('nodejs_mongodb_crud');
-  let myObj = { name: 'Company Inc', address: 'Highway 37'};
-  dbo.collection("customers").insertOne(myObj, (err, res) => {
+
+  // insert single document
+  // let myObj = { name: 'Company Inc', address: 'Highway 37'};
+  // dbo.collection("customers").insertOne(myObj, (err, res) => {
+  //   if (err) throw err;
+  //   console.log('1 document inserted');
+  //   db.close();
+  // });
+
+  // insert multiple documents
+  let myObj = [
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'},
+    { name: 'Hannah', address: 'Mountain 21'},
+    { name: 'Michael', address: 'Valley 345'},
+    { name: 'Sandy', address: 'Ocean blvd 2'},
+    { name: 'Betty', address: 'Green Grass 1'},
+    { name: 'Richard', address: 'Sky st 331'},
+    { name: 'Susan', address: 'One way 98'},
+    { name: 'Vicky', address: 'Yellow Garden 2'},
+    { name: 'Ben', address: 'Park Lane 38'},
+    { name: 'William', address: 'Central st 954'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'Viola', address: 'Sideway 1633'}
+  ];
+
+  dbo.collection("customers").insertMany(myObj, function(err, res) {
     if (err) throw err;
-    console.log('1 document inserted');
+    console.log("Number of documents inserted: " + res.insertedCount);
+    console.log(res);
     db.close();
   });
 });
