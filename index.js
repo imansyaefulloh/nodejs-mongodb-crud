79e923d9697bb0f1cc2a3e1b59b0be55eada2dbb
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 21:55:28 +0700

    join

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ MongoClient.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
   if (err) throw err;
   let dbo = db.db('nodejs_mongodb_crud');
 
-  dbo.collection("customers").find().limit(5).toArray(function(err, result) {
+  dbo.collection('orders').aggregate([
+    { $lookup:
+       {
+         from: 'products',
+         localField: 'product_id',
+         foreignField: '_id',
+         as: 'orderdetails'
+       }
+     }
+    ]).toArray(function(err, res) {
     if (err) throw err;
-    console.log(result);
+    console.log(JSON.stringify(res));
     db.close();
   });
 });
