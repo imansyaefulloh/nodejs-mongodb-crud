e515e95f6fa75d2317191f178021225c87e48267
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 21:30:16 +0700

    creating collection

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 
-const mongoURI = 'mongodb://localhost:27017/nodejs_mongodb_crud';
+const mongoURI = 'mongodb://localhost:27017/';
 
 MongoClient.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
   if (err) throw err;
-  console.log('Database created');
-  db.close();
+  let dbo = db.db('nodejs_mongodb_crud');
+  dbo.createCollection("customers", (err, res) => {
+    if (err) throw err;
+    console.log('Collection created');
+    db.close();
+  });
 });
